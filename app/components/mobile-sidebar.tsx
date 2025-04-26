ab2fd10fcafe38f55f7e8d826bf00cc5045b1729
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-04-26 10:12:28 +0530

    feat(navigation): mobile-sidebar and nav-items

--- app/components/mobile-sidebar.tsx
+++ app/components/mobile-sidebar.tsx
@@ -1,7 +1,41 @@
+// @ts-nocheck
+
+import { Link } from "react-router";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { NavItems } from "~/components";
+
 const MobileSidebar = () => {
-    return (
-      <div className="">MobileSidebar</div>
-    )
+  let sidebar: SidebarComponent;
+
+  const toggleSidebar = () => {
+    sidebar.toggle()
   }
-  
-  export default MobileSidebar;
+
+  return (
+    <div className="mobile-sidebar wrapper">
+      <header>
+        <Link to="/">
+          <img src="/assets/icons/logo.svg" alt="Logo" className="size-[30px]" />
+          <h1>Tourvisto</h1>
+        </Link>
+
+        <button onClick={toggleSidebar}>
+          <img src="/assets/icons/menu.svg" alt="Menu" className="size-7" />
+        </button>
+      </header>
+
+      <SidebarComponent
+        width={270}
+        ref={(Sidebar) => sidebar = Sidebar}
+        created={() => sidebar.hide()}
+        closeOnDocumentClick={true}
+        showBackdrop={true}
+        type="over"
+      >
+        <NavItems handleClick={toggleSidebar} />
+      </SidebarComponent>
+    </div>
+  )
+}
+
+export default MobileSidebar;
