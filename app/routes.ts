f8ef60c186aedb445738914f77a8afe07ae010dd
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-04-27 11:49:40 +0530

    feat(routes): layout/route for sign-in/other pages

--- app/routes.ts
+++ app/routes.ts
@@ -3,16 +3,27 @@ import {
   index,
   layout,
   route,
+  prefix,
 } from "@react-router/dev/routes";
 
 export default [
   layout("routes/admin/admin-layout.tsx", [
     route("dashboard", "routes/admin/dashboard.tsx"),
     route("all-users", "routes/admin/all-users.tsx"),
-    route("trips", "routes/admin/trips.tsx"),
-    route("trips/create", "routes/admin/create-trip.tsx"),
-    route("trips/:tripId", "routes/admin/trip-detail.tsx"),
+    ...prefix("trips", [
+      index("routes/admin/trips.tsx"),
+      route("create", "routes/admin/create-trip.tsx"),
+      route(":tripId", "routes/admin/trip-detail.tsx"),
+    ]),
   ]),
-
-  index("routes/home.tsx"),
+  layout("routes/root/root-layout.tsx", [
+    index("routes/root/travel-redirect.tsx"),
+    ...prefix("travel", [
+      index("routes/root/travel-page.tsx"),
+      route(":tripId", "routes/root/travel-detail.tsx"),
+      route(":tripId/success", "routes/root/payment-success.tsx"),
+    ]),
+  ]),
+  route("sign-in", "routes/root/sign-in.tsx"),
+  route("api/create-trip", "routes/api/create-trip.ts"),
 ] satisfies RouteConfig;
